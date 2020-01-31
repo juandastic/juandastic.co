9adb1d6ae774b4c526ed25bd0ffae5e13372133e
Juan David Gomez <juandastic@gmail.com>
2020-01-31 01:17:47 -0500

    first commit

---
+++ web1/components/Post/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+import Wrapper from './Wrapper'
+
+const PostItem = ({ post }) => (
+  <Wrapper>
+    <Link href="/post/[slug]" as={`/post/${post.title}`}>
+      <a>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </a>
+    </Link>
+  </Wrapper>
+)
+
+export default PostItem
