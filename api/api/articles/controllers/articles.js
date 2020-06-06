5456f26ebfe0712cdd43f674326e474d7a63068c
Juan David Gomez <juandastic@gmail.com>
2020-06-06 21:08:43 +0000

    fix error on empty article

--- api/api/articles/controllers/articles.js
+++ api/api/articles/controllers/articles.js
@@ -16,10 +16,9 @@ module.exports = {
   async findOne(ctx) {
     const entity = await strapi.services.articles.findOne({slug: ctx.params.slug});
 
-    if(ctx.query["project.project_id"] !== entity.project.project_id)
-      return false;
-
     if (entity) {
+      if(ctx.query["project.project_id"] !== entity.project.project_id)
+        return false;
       const articles_related = await strapi.services.articles.find({ id_ne: entity.id, project: entity.project.id});
 
       entity.articles_related = articles_related
