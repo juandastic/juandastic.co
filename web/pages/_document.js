9e41711a9c9474f60f76296551c04986e22cb06b
Juan David Gomez <juandastic@gmail.com>
2021-04-04 17:23:16 -0500

    include styles from server side

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -2,7 +2,7 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class SiteDocument extends Document {
-    // script contents onto page
+  // script contents onto page
   setGoogleTags() {
     return {
       __html: `
@@ -14,20 +14,43 @@ export default class SiteDocument extends Document {
     };
   }
 
-  render () {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
-    const main = sheet.collectStyles(<Main />)
-    const styleTags = sheet.getStyleElement()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      })
+
+      // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+      const initialProps = await Document.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render () {
     return (
       <html lang="es">
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          {styleTags}
         </Head>
         <body>
           <div className="root">
-            {main}
+            <Main />
           </div>
           <NextScript />
           <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138270849-1"></script>
